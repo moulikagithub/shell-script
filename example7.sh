f3e81c169fc2fa5a9ff3e7182f90de7f87880f6f
User <kalidindi.moulika@gmail.com>
2023-12-12 12:59:37 -0600

    example7 practice2

--- example7.sh
+++ example7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FRUITS=$("Apples","bananas","peaches")
+FRUITS=("Apples","bananas","peaches")
 echo "first fruit is:${FRUITS[0]}"
 echo "second fruit is:${FRUITS[1]}"
 echo "third fruit is:${FRUITS[3]}"
