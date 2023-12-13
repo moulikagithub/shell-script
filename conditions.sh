7bedf1f81f193dd13cb8605c8ab56518418388a8
User <kalidindi.moulika@gmail.com>
2023-12-13 08:48:13 -0600

    commiting for practice

--- conditions.sh
+++ conditions.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-number=$1
-if( number -ge 100 )
-    {
-      echo "given number $number is greater than 100"
-    }
-else
-{
-    echo "given number $number is lessthan 100"
-}
 
+number=$1
+
+if [ $number -gt 100 ]
+then
+    echo "given number $number is greater than 100"
+else
+    echo "given number $number is lessthan 100"
 fi
