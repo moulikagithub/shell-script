d665a177eeb8d89f4ed2d85e58166c87b6a60902
User <kalidindi.moulika@gmail.com>
2023-12-19 16:59:07 -0600

    practice

--- deletinglog.sh
+++ deletinglog.sh
@@ -13,8 +13,14 @@ file_delete=$(find $source -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
+   if [ $? -ne 0 ]
+   then
+       echo "no 14 days old log files avaliable"
+    else
     echo "deleting more than 14 days old logs from $source:$line"
-    rm -rf $line
+   # rm -rf $line
+   fi
+
     
 done <<< $file_delete
  
